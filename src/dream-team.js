9a31aa61112a944bc81540729abe3b2d16321d0e
Egor Dubovik <Hamelion74@gmail.com>
2020-03-22 23:52:35 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  // throw 'Not implemented';
+  return members instanceof Array
+  ? members
+      .filter(member => typeof member === 'string')
+      .reduce((secret, member) => secret += member.trim()[0], '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('')
+      .toUpperCase()
+  : false;
 };
